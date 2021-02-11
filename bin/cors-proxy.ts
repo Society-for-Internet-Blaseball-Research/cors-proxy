edac443da781f72ac0017777a3322b8d95487dca
iliana etaoin <iliana@buttslol.net>
2021-02-10 22:23:33 -0800

    byeeeee

--- bin/cors-proxy.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { CorsProxyStack } from '../lib/cors-proxy-stack';
-
-const app = new cdk.App();
-new CorsProxyStack(app, 'CorsProxyStack', {
-  domainName: 'cors-proxy.blaseball-reference.com',
-});
